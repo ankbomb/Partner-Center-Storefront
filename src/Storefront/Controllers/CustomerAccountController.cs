d9265972f0af3b4ab6dba0a699b9bc00b529cb86
ankbomb <64812947+ankbomb@users.noreply.github.com>
2020-05-09 13:18:44 +0200

    monthly
    monthly

--- src/Storefront/Controllers/CustomerAccountController.cs
+++ src/Storefront/Controllers/CustomerAccountController.cs
@@ -152,9 +152,14 @@ namespace Microsoft.Store.PartnerCenter.Storefront.Controllers
                     isEditable = false;
                 }
 
-                // Compute the pro rated price per seat for this subcription & return for client side processing during updates. 
-                decimal proratedPerSeatPrice = Math.Round(CommerceOperations.CalculateProratedSeatCharge(subscription.ExpiryDate, portalOfferPrice), responseCulture.NumberFormat.CurrencyDecimalDigits);
+                BrandingConfiguration portalBranding = await ApplicationDomain.Instance.PortalBranding.RetrieveAsync().ConfigureAwait(false);
 
+                // Compute the pro rated price per seat for this subcription & return for client side processing during updates.
+                decimal proratedPerSeatPrice = Math.Round(CommerceOperations.CalculateProratedSeatCharge(subscription.ExpiryDate,
+                                                                                                         portalOfferPrice,
+                                                                                                         portalBranding.BillingCycle),
+                                                          Resources.Culture.NumberFormat.CurrencyDecimalDigits);
+                                                          
                 SubscriptionViewModel subscriptionItem = new SubscriptionViewModel()
                 {
                     SubscriptionId = subscription.SubscriptionId,

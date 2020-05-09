195d247a14b9c019dc39a7b329e60467d0d63dc6
ankbomb <64812947+ankbomb@users.noreply.github.com>
2020-05-09 13:26:12 +0200

    Create BillingCycleType.cs
    adding monthly option

---
+++ src/Storefront/Models/BillingCycleType.cs
@@ -0,0 +1,14 @@
+// -----------------------------------------------------------------------
+// <copyright file="BrandingConfiguration.cs" company="Microsoft">
+//      Copyright (c) Microsoft Corporation. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Microsoft.Store.PartnerCenter.Storefront.Models
+{
+    public enum BillingCycleType
+    {
+        Annual = 0,
+        Monthly = 1
+    }
+} 
